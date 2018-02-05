0fa2ad5b99b0942d6bea3cb085f1f7ed7f542ac2
Billy Troy Wooton <gcowootont@gmail.com>
2018-02-05 12:43:10 -1000

    Finished ziplist

---
+++ ziplist.js
@@ -0,0 +1,34 @@
+function zipList(l1, l2) {
+  const result = [];
+  let innerl1 = l1;
+  let innerl2 = l2;
+  let current = innerl1;
+
+  while (innerl1.length > 0 && innerl2.length > 0) {
+    if (current === innerl1) {
+      result.push(l1[0]);
+      innerl1 = innerl1.slice(1);
+      current = innerl2;
+    } else {
+      result.push(l2[0]);
+      innerl2 = innerl2.slice(1);
+      current = innerl1;
+    }
+  }
+  while (innerl1.length > 0) {
+    result.push(l1[0]);
+    innerl1 = innerl1.slice(1);
+  }
+  while (innerl2.length > 0) {
+    result.push(l2[0]);
+    innerl2 = innerl2.slice(1);
+  }
+  return result;
+}
+
+function zipListTheSimpleWay(l1, l2) {
+  return _.flatten(_.zip(l1, l2));
+}
+
+console.log(zipList(['a', 'b', 'c'], [1, 2, 3]));
+console.log(zipListTheSimpleWay(['a', 'b', 'c'], [1, 2, 3]));
